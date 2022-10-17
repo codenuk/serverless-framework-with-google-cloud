6abefa096813312e373953b69f101d38f0158356
codenuk <supagorn00001@gmail.com>
2022-10-17 21:02:20 +0700

    done

--- index.js
+++ index.js
@@ -9,13 +9,22 @@ const mysql = require("serverless-mysql")({
   },
 });
 
-exports.http = async (request, response) => {
+exports.hello = async (request, response) => {
+  // let results = await mysql.query('SELECT * FROM User')
+  // console.log('results', results)
+  // // Run clean up function
+  // await mysql.end()
+
+  response.status(200).send("hello");
+};
+
+exports.helloNuk = async (request, response) => {
   let results = await mysql.query('SELECT * FROM User')
   console.log('results', results)
   // Run clean up function
   await mysql.end()
 
-  response.status(200).send(results);
+  response.status(200).json(JSON.parse(result.body));
 };
 
 exports.event = (event, callback) => {
